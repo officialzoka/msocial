ae18bea1a6d065858a79ecf1f1461bdfd87dec21
officialzoka <golldenhouse@gmail.com>
2022-07-16 14:37:33 +0200

    [fix deployment errors #2]

--- index.js
+++ index.js
@@ -12,10 +12,17 @@ require('dotenv').config();
 
 const AuthRoutes = require('./routes/auth.route');
 
-mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(
+    process.env.DB_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) => {
+        if (err) throw err;
+        console.log('Connected to MongoDB');
+    },
+);
 
 const app = express();
 app.use(function (req, res, next) {
@@ -47,4 +54,7 @@ app.use(cors());
 
 app.use('/api/auth', AuthRoutes);
 
-app.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
